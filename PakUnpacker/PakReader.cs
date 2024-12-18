01f4cb628ce3c89a0d6f086db143dfd671fbc9f2
RoboGuru <114884227+Rendszerguru@users.noreply.github.com>
2024-12-18 23:35:19 +0100

    Update PakReader.cs

--- PakUnpacker/PakReader.cs
+++ PakUnpacker/PakReader.cs
@@ -9,28 +9,62 @@ class PakReader : BinaryReader
 
 	public void SkipSignature()
 	{
-		BaseStream.Position += 4;
+		if (CanRead(4))
+		{
+			BaseStream.Position += 4;
+		}
+		else
+		{
+			Console.WriteLine("Warning: Not enough data to skip the signature.");
+			BaseStream.Position = BaseStream.Length;
+		}
 	}
 	public bool CanRead(int length)
 	{
-		return BaseStream.Position + length <= BaseStream.Length;
+		return length >= 0 && BaseStream.Position + length <= BaseStream.Length;
 	}
 	public void Skip(int count)
 	{
-		BaseStream.Position += count;
+		if (CanRead(count))
+		{
+			BaseStream.Position += count;
+		}
+		else
+		{
+			Console.WriteLine($"Warning: Attempt to skip {count} bytes, but not enough data remains. Skipping to the end of the stream.");
+			BaseStream.Position = BaseStream.Length;
+		}
 	}
-
 	public long Pos() => BaseStream.Position;
 
 	public int ReadInt32BE()
 	{
-		var data = ReadBytes(4);
-		Array.Reverse(data);
-		return BitConverter.ToInt32(data, 0);
+		if (CanRead(4))
+		{
+			var data = ReadBytes(4);
+			Array.Reverse(data);
+			return BitConverter.ToInt32(data, 0);
+		}
+		else
+		{
+			Console.WriteLine("Warning: Not enough data to read 4 bytes for an Int32.");
+			return 0;
+		}
 	}
-
 	public string ReadStringUtf8(int length)
 	{
+		if (length <= 0)
+		{
+			Console.WriteLine("Warning: Attempted to read a string with non-positive length.");
+			return string.Empty;
+		}
+
+		if (!CanRead(length))
+		{
+			Console.WriteLine($"Warning: Not enough data to read a string of length {length}.");
+			return string.Empty;
+		}
+
 		var bytes = ReadBytes(length);
 		try
 		{
@@ -47,4 +81,5 @@ class PakReader : BinaryReader
 			return "InvalidEntry";
 		}
 	}
+
 }
