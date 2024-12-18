e607569c49ff54bbb99b0ae78861f75b169985e3
RoboGuru <114884227+Rendszerguru@users.noreply.github.com>
2024-12-19 00:09:06 +0100

    Update Program.cs

--- PakUnpacker/Program.cs
+++ PakUnpacker/Program.cs
@@ -1,6 +1,15 @@
-﻿using PakReaderExe;
+using PakReaderExe;
 using System;
 using System.IO;
+using System.Reflection;
+
+// Display the application's version
+string version = System.Reflection.Assembly.GetExecutingAssembly()
+            .GetName()
+            .Version
+            .ToString(3);
+
+Console.WriteLine($"Application Version: {version}");
 
 // Find all .pak files in the current directory
 string[] pakFiles = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.pak");
