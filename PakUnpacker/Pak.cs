51f5c3f98b0f94bcf1bcdaadd7ee6df4e4aaecbb
RoboGuru <114884227+Rendszerguru@users.noreply.github.com>
2024-12-18 22:07:18 +0100

    Update Pak.cs

--- PakUnpacker/Pak.cs
+++ PakUnpacker/Pak.cs
@@ -1,19 +1,21 @@
-﻿namespace PakReaderExe;
+namespace PakReaderExe;
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 class Pak
 {
 	public enum EntryType
-    {
-        Directory, File1
-    }
+	{
+		Directory, File1
+	}
 
 	public string name;
 	public int formSize;
 	public int dataSize;
 	public int entriesSize;
 	public List<PakEntryFile> entries = new();
-	
+
 	public Pak(string src)
 	{
 		name = src;
@@ -24,102 +26,170 @@ class Pak
 		ReadData(pr);
 		ReadEntries(pr);
 	}
-    
-    public void ReadForm( PakReader pr )
-    {
+
+	public void ReadForm(PakReader pr)
+	{
 		pr.SkipSignature();
 		formSize = pr.ReadInt32BE();
 		pr.SkipSignature();
-    }
-    
-    public void ReadHead( PakReader pr)
-    {
+	}
+
+	public void ReadHead(PakReader pr)
+	{
 		pr.SkipSignature();
 		Console.WriteLine("Head of the Pak File:" + Convert.ToBase64String(pr.ReadBytes(32)));
 	}
-    
-    public void ReadData( PakReader pr)
-    {
+
+	public void ReadData(PakReader pr)
+	{
 		pr.SkipSignature();
 		dataSize = pr.ReadInt32BE();
 		pr.Skip(dataSize);
-    }
+	}
 
-    public void ReadEntries(PakReader pr)
-    {
+	public void ReadEntries(PakReader pr)
+	{
 		pr.SkipSignature();
 		entriesSize = pr.ReadInt32BE();
 
-        try
+		try
 		{
 			pr.Skip(2);
 			pr.Skip(4);
-            
-            for ( long posEntries = pr.Pos(); pr.Pos() - posEntries < entriesSize; )
-            {
+
+			long posEntries = pr.Pos();
+			while (pr.Pos() - posEntries < entriesSize)
+			{
+				try
+				{
+					// Check if there's enough data to read
+					if (!pr.CanRead(2))
+					{
+						//Console.WriteLine("Warning: Can't read further as the stream end is reached.");
+						break;
+					}
+
+					EntryType entryType = (EntryType)pr.ReadByte();
+					int entryNameLength = pr.ReadByte();
+
+					if (!pr.CanRead(entryNameLength))
+					{
+						Console.WriteLine("Warning: The entry name is too long for the remaining data.");
+						break;
+					}
+
+					string entryName = pr.ReadStringUtf8(entryNameLength);
+
+					if (entryType == EntryType.Directory)
+					{
+						ReadEntriesFromDirectory(entryName, pr);
+					}
+					else
+					{
+						entries.Add(new(entryName, pr));
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"Error while reading an entry: {ex.Message}");
+					break; // Exit the loop on error
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"Error while processing the entries: {ex.Message}");
+		}
+	}
+
+	public void ReadEntriesFromDirectory(string dirName, PakReader pr)
+	{
+		if (!pr.CanRead(4))
+		{
+			Console.WriteLine("Warning: Can't read directory entry as there is not enough data.");
+			return;
+		}
+
+		int childCount = pr.ReadInt32();
+
+		for (int i = 0; i < childCount; i++)
+		{
+			try
+			{
+				if (!pr.CanRead(2))
+				{
+					Console.WriteLine("Warning: The directory entry is too short.");
+					break;
+				}
+
 				EntryType entryType = (EntryType)pr.ReadByte();
 				int entryNameLength = pr.ReadByte();
-				string entryName = new(pr.ReadChars(entryNameLength));
 
-				if (entryType == EntryType.Directory) ReadEntriesFromDirectory(entryName, pr);
-				else entries.Add(new(entryName, pr));
-            }
+				if (!pr.CanRead(entryNameLength))
+				{
+					Console.WriteLine("Warning: The directory entry name is too long.");
+					break;
+				}
+
+				string entryName = dirName + "\\" + pr.ReadStringUtf8(entryNameLength);
+
+				if (entryType == EntryType.File1)
+				{
+					entries.Add(new(entryName, pr));
+				}
+				else if (entryType == EntryType.Directory)
+				{
+					ReadEntriesFromDirectory(entryName, pr);
+				}
+				else
+				{
+					Console.WriteLine("Unknown entry type.");
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Error while reading a directory entry: {ex.Message}");
+				break;
+			}
 		}
-        catch { }
-    }
-    
-    public void ReadEntriesFromDirectory(string dirName, PakReader pr)
-    {
-        int childCount = pr.ReadInt32();
-        
-        for ( int i = 0; i < childCount; i++ )
-        {
-            EntryType entryType = (EntryType)pr.ReadByte();
-            int entryNameLength = pr.ReadByte();
-            string entryName = dirName + "\\" + new string(pr.ReadChars(entryNameLength));
+	}
 
-            if ( entryType == EntryType.File1 )
-            {
-                entries.Add(new(entryName, pr));
-            }
-            else if ( entryType == EntryType.Directory )
-            {
-                ReadEntriesFromDirectory(entryName, pr);
-            }
-            else
-            {
-                throw new Exception("Unbekannter Entry Typ");
-            }
-        }
-    }
+	public void ExtractDataBlock(string dst)
+	{
+		using PakReader pr = new(File.OpenRead(name));
 
-    public void ExtractDataBlock( string dst)
-    {
-        using PakReader pr = new(File.OpenRead(name));
-        foreach (PakEntryFile entry in entries)
-        {
-            string? dir = Path.GetDirectoryName(dst + "\\" + entry.name);
-            if (dir is not null) Directory.CreateDirectory(dir);
+		foreach (PakEntryFile entry in entries)
+		{
+			try
+			{
+				string? dir = Path.GetDirectoryName(dst + "\\" + entry.name);
+				if (dir is not null) Directory.CreateDirectory(dir);
 
-            if (entry.compression == PakEntryFile.CompressionType.Zlib)
-            {
-                try
-                {
-                    pr.BaseStream.Position = entry.offset;
-                    Compression.Zlib.Decompress(pr, entry.originalSize, dst + "\\" + entry.name);
-                }
-                catch
-                {
-                    pr.BaseStream.Position = entry.offset;
-                    File.WriteAllBytes(dst + "\\" + entry.name + "_DekomprimierungFehlgeschlagen", pr.ReadBytes(entry.size));
-
-                }
-            }
-            else
-            {
-                pr.BaseStream.Position = entry.offset;
-                File.WriteAllBytes(dst + "\\" + entry.name, pr.ReadBytes(entry.size));
-            }
-        }
-    }
+				if (entry.compression == PakEntryFile.CompressionType.Zlib)
+				{
+					try
+					{
+						pr.BaseStream.Position = entry.offset;
+						Compression.Zlib.Decompress(pr, entry.originalSize, dst + "\\" + entry.name);
+					}
+					catch
+					{
+						Console.WriteLine($"Error during Zlib decompression: {entry.name}");
+						pr.BaseStream.Position = entry.offset;
+						File.WriteAllBytes(dst + "\\" + entry.name + "_Error", pr.ReadBytes(entry.size));
+					}
+				}
+				else
+				{
+					pr.BaseStream.Position = entry.offset;
+					File.WriteAllBytes(dst + "\\" + entry.name, pr.ReadBytes(entry.size));
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Error while extracting a file ({entry.name}): {ex.Message}");
+				continue;
+			}
+		}
+	}
 }
