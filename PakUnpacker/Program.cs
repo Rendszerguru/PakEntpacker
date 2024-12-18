e6b6ae967982a8eb2caf5a49fdf6c1eba2845d31
RoboGuru <114884227+Rendszerguru@users.noreply.github.com>
2024-12-19 00:09:48 +0100

    Update Program.cs

--- PakUnpacker/Program.cs
+++ PakUnpacker/Program.cs
@@ -5,9 +5,9 @@ using System.Reflection;
 
 // Display the application's version
 string version = System.Reflection.Assembly.GetExecutingAssembly()
-            .GetName()
-            .Version
-            .ToString(3);
+			.GetName()
+			.Version
+			.ToString(3);
 
 Console.WriteLine($"Application Version: {version}");
 
@@ -17,20 +17,20 @@ string[] pakFiles = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.pak")
 // If no .pak files are found, display a message and exit
 if (pakFiles.Length == 0)
 {
-    Console.WriteLine("No '.pak' files found in the directory.");
-    return;
+	Console.WriteLine("No '.pak' files found in the directory.");
+	return;
 }
 
 // Iterate over each .pak file
 foreach (string pakPath in pakFiles)
 {
-    Console.WriteLine($"Unpacking: {pakPath}");
+	Console.WriteLine($"Unpacking: {pakPath}");
 
-    // Create a new Pak object for the current file
-    Pak pak = new(pakPath);
+	// Create a new Pak object for the current file
+	Pak pak = new(pakPath);
 
-    // Extract the data block to a directory with the same name as the .pak file
-    pak.ExtractDataBlock(Path.ChangeExtension(pakPath, null));
+	// Extract the data block to a directory with the same name as the .pak file
+	pak.ExtractDataBlock(Path.ChangeExtension(pakPath, null));
 }
 
 Console.WriteLine("Done!");
